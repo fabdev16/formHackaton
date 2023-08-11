e18a7c8864d5ddeba2684a8885e98061604c726c
Jean Soto <jpsotochirinos@gmail.com>
2023-08-11 18:03:43 -0500

    Docu

--- src/proxy/mock.js
+++ src/proxy/mock.js
@@ -1,4 +1,4 @@
-import credentials from './credentials/auth.js';
+import credentials from './credentials/auth.js'; // TODO: obtener desde un api de autorizacion
 
 export const getCredentials = (identifier) => {
   return credentials[identifier] || null;
