9ddec93a21e3ddb03d1da6a4ecdee771c85d37e2
Jean Soto <jpsotochirinos@gmail.com>
2023-08-11 17:19:35 -0500

    clean

---
+++ src/proxy/credentials/auth.js
@@ -0,0 +1,23 @@
+const credentials = {
+        "jeanspierre100": {
+        "accountName": "jeanspierre100",
+        "environment": "vtexcommercestable",
+        "appKey": "vtexappkey-jeanspierre100-BCXQMF",
+        "appToken": "TQGWQPAQTROYDFGHTSOIEXUQJYRYDQLBMZYWJVYSLYBCOBJJTMZQIIHQEPIYXDTIRXGWNQZQPOVVPNCJKESYQPZTBZLDOLSVXJFVLKHLGXWBMFOGACYBENHMRZNEDJBW"
+        },
+        "selleralli448": {
+        "accountName": "selleralli448",
+        "environment": "vtexcommercestable",
+        "appKey": "vtexappkey-selleralli448-ZQGGET",
+        "appToken": "WCMUGTUTYRTFSVQEKMKIRISXJLZAHLYCSOORIHTIWOOEZLFAGGWJNNEJXOTTCNODRKWLOPTZHOPBDNFIPNDWSMVQJDNNSSYFWWIGJKTXXDARCHTVLJIHKJUQRFOYYTTQ"
+        },
+        "sellerprodequa1880": {
+        "accountName": "sellerprodequa1880",
+        "environment": "vtexcommercestable",
+        "appKey": "vtexappkey-sellerprodequa1880-GQKMTG",
+        "appToken": "FGEYGXEFOVPMZQNOAOSUAQNWQVWLWYYUBDKWJBUPAHSONLOOAJOJGHZQQZBNNLFHTPDGWTRGFYWYUVZQAAQZWJYWTGGATJTVMUUYWTPZZIZASUKRCQWJYUJRYCVBWOBL"
+        }
+    };
+  
+  
+  export default credentials;
